84c0038b7e1c6c8adaed89a88a4ca8ed1a38dcf8
aprilyana <imelaprilyanasafari@gmail>
2024-07-22 20:10:14 +0700

    add login controller kodular

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,18 +1,21 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
+
 use App\Http\Controllers\Auth\Session;
 
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+
 class LoginController extends Controller
 {
     public function login()
     {
         if (Auth::check()) {
             return redirect('dashboard');
-        }else{
+        } else {
             return view('index2');
         }
     }
@@ -39,5 +42,27 @@ class LoginController extends Controller
         Auth::logout();
         session()->flush();
         return redirect('/');
+    }
+
+    public function kodularlogin(Request $request)
+    {
+        $credentials = $request->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        if (Auth::Attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->route('dashboard')
+                ->withSuccess('You have successfully logged in!');
+        }
+        $user = User::where('username', $request->username)->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
     }
 }
