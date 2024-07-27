41fb457c313ba8dd04dfa1199ea109f53f7a01c5
aprilyana <imelaprilyanasafari@gmail>
2024-07-27 16:30:43 +0700

    handle data sent

--- app/Http/Controllers/SendDataController.php
+++ app/Http/Controllers/SendDataController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\WAQMS_Raw;
+use App\Models\WAQMS_Valid;
 use Illuminate\Http\Request;
 
 class SendDataController extends Controller
 {
-    //
+    public function send(Request $request)
+    {
+        $data = $request->all();
+
+        // Create entry in WAQMS_Raw
+        WAQMS_Raw::create($data);
+
+        // Nullify values outside specified ranges
+        if (isset($data['tvoc']) && ($data['tvoc'] < 0 || $data['tvoc'] > 60000)) {
+            $data['tvoc'] = null;
+        }
+        if (isset($data['eco2']) && ($data['eco2'] < 400 || $data['eco2'] > 60000)) {
+            $data['eco2'] = null;
+        }
+        if (isset($data['pm25']) && ($data['pm25'] < 0 || $data['pm25'] > 1000)) {
+            $data['pm25'] = null;
+        }
+        if (isset($data['humidity']) && $data['humidity'] >= 80 && isset($data['pm25'])) {
+            $data['pm25'] = $data['pm25'] * 0.67;
+        }
+        if (isset($data['pm10']) && ($data['pm10'] < 0 || $data['pm10'] > 1000)) {
+            $data['pm10'] = null;
+        }
+        if (isset($data['temp']) && ($data['temp'] < -40 || $data['temp'] > 85)) {
+            $data['temp'] = null;
+        }
+        if (isset($data['humidity']) && ($data['humidity'] < 0 || $data['humidity'] > 100)) {
+            $data['humidity'] = null;
+        }
+        if (isset($data['pressure']) && ($data['pressure'] < 300 || $data['pressure'] > 1100)) {
+            $data['pressure'] = null;
+        }
+
+        // Create entry in WAQMS_Valid
+        WAQMS_Valid::create($data);
+
+        return response()->json(['message' => 'Data processed successfully.']);
+    }
 }
