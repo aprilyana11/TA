87c3fd221ca22c3ec8980643048afc45d884dcfb
aprilyana <imelaprilyanasafari@gmail>
2024-07-30 16:58:08 +0700

    p

--- resources/views/index2.blade.php
+++ resources/views/index2.blade.php
@@ -6,7 +6,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
-  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
   <link rel="stylesheet" href="{{ asset('css/style2.css') }}">
   <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
   <script src="myscript.js" type="module"></script>
